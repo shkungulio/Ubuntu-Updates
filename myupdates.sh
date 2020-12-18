f4bdc18d9acd51ea1890528f91688a4928d26400
Seif Kungulio <shkungulio@gmail.com>
2020-12-18 10:33:16 -0600

    Condensed the echo lines

--- myupdates.sh
+++ myupdates.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-echo " "
-echo "********** Start Updating... **********"
+
+echo " "; echo "********** Start Updating... **********"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get autoclean -y
 sudo apt-get autoremove -y
-echo " "
-echo "********** Updates Completed Successfully **********"
+echo " "; echo "********** Updates Completed Successfully **********"
